5b85e9c6c519dbd7b45d8df9da533d50aaf3769c
mirror@stealer.net <mirror@stealer.net>
2013-07-13 19:30:35 +0000

    2013-07-13 19:06:56+00:00

--- app-office/libreoffice/files/generate_libreoffice_package.sh
+++ app-office/libreoffice/files/generate_libreoffice_package.sh
@@ -5,8 +5,8 @@
 #  * for amd64: CFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -g"
 
 # What you can set:
-VERSION="3.6.6.2"
-BINVERSION="3.6.6.2"
+VERSION="4.0.4.2"
+BINVERSION="4.0.4.2"
 OPTS="-v"
 USEFILE="/etc/portage/package.use/libreo"
 MYPKGDIR="$( portageq pkgdir )"
@@ -44,7 +44,7 @@ do
 done
 
 # first the default subset of useflags
-IUSES_BASE="bash-completion branding cups dbus gstreamer gtk opengl vba webdav -aqua -binfilter -jemalloc -mysql -odk -postgres"
+IUSES_BASE="bash-completion branding cups dbus gstreamer gtk opengl vba webdav -aqua -binfilter -bluetooth -debug -jemalloc -mysql -odk -postgres -telepathy"
 
 ENABLE_EXTENSIONS="presenter-console presenter-minimizer"
 DISABLE_EXTENSIONS="nlpsolver pdfimport scripting-beanshell scripting-javascript wiki-publisher"
