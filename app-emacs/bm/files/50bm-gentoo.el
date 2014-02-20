23a66885c64347602a57e9ebbbb7cc3e7505fbd5
mirror@stealer.net <mirror@stealer.net>
2014-02-20 08:31:21 +0000

    2014-02-20 08:07:02+00:00

--- app-emacs/bm/files/50bm-gentoo.el
+++ app-emacs/bm/files/50bm-gentoo.el
@@ -1,6 +1,3 @@
-
-;;; bm site-lisp configuration
-
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'bm-toggle "bm" "Toggle bookmark in current buffer." t)
 (autoload 'bm-next "bm" "Goto bookmark." t)
