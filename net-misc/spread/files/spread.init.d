646b017c72e7044461b11786633258978f292c45
mirror@stealer.net <mirror@stealer.net>
2014-03-11 10:31:21 +0000

    2014-03-11 10:07:00+00:00

--- net-misc/spread/files/spread.init.d
+++ net-misc/spread/files/spread.init.d
@@ -1,21 +1,26 @@
 #!/sbin/runscript
-# Copyright 1999-2005 Gentoo Foundation
+# Copyright 1999-2014 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/net-misc/spread/files/spread.init.d,v 1.1 2006/10/06 12:48:29 caleb Exp $
-# $Id: spread.init.d,v 1.1 2006/10/06 12:48:29 caleb Exp $
+# $Header: /var/cvsroot/gentoo-x86/net-misc/spread/files/spread.init.d,v 1.2 2014/03/11 09:50:45 kumba Exp $
+# $Id: spread.init.d,v 1.2 2014/03/11 09:50:45 kumba Exp $
 
 depend() {
-  need net
+	need net
+}
+
+mkdir_spreadirs() {
+	[ -d /var/run/spread ] || mkdir -p /var/run/spread
 }
 
 start() {
-  ebegin "Starting Spread Daemon"
-  start-stop-daemon --start --quiet --background --make-pidfile --pidfile /var/run/spread.pid --exec /usr/sbin/spread &
-  eend $?
+	ebegin "Starting Spread Daemon"
+	mkdir_spreadirs
+	start-stop-daemon --start --quiet --background --make-pidfile --pidfile /var/run/spread.pid --exec /usr/sbin/spread &
+	eend $?
 }
 
 stop() {
-  ebegin "Stopping Spread"
-  start-stop-daemon --stop --pidfile /var/run/spread.pid
-  eend $?
+	ebegin "Stopping Spread"
+	start-stop-daemon --stop --pidfile /var/run/spread.pid
+	eend $?
 }
