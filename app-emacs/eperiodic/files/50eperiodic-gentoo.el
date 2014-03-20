fca4720ddc43c4fa81cd8bae3ed7f18d8787f28b
mirror@stealer.net <mirror@stealer.net>
2014-03-20 09:01:25 +0000

    2014-03-20 08:37:01+00:00

--- app-emacs/eperiodic/files/50eperiodic-gentoo.el
+++
@@ -1,7 +0,0 @@
-
-;;; eperiodic site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'eperiodic "eperiodic"
-  "Display the periodic table of the elements in its own buffer"
-  t)
