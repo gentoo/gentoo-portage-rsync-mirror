569c60f25cd5a0bda01d7d1e128e9cd7d564df33
mirror@stealer.net <mirror@stealer.net>
2014-03-29 14:31:22 +0000

    2014-03-29 14:07:00+00:00

--- www-client/firefox-bin/files/all-gentoo-1.js
+++ www-client/firefox-bin/files/all-gentoo-1.js
@@ -4,7 +4,7 @@ lockPref("app.update.enabled", false);
 lockPref("intl.locale.matchOS",                true);
 // Allow user to change based on needs
 defaultPref("browser.display.use_system_colors",  true);
-defaultPref("spellchecker.dictionary_path", "/usr/share/myspell"
+defaultPref("spellchecker.dictionary_path", "/usr/share/myspell");
 defaultPref("browser.shell.checkDefaultBrowser",  false); 
 // Preferences that should be reset every session
 pref("browser.EULA.override",              true); 
